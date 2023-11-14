9d3dfa24507950b67a7272f69ba04718f9dcdffa
Rogerio Bispo <silva_rogeriobispo@outlook.com>
2023-11-14 19:41:52 -0300

    ativações javascript atz1

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// ativar underline menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ativar produtos orçamento
+
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+// ativar perguntas
+
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventoPerguntas);
