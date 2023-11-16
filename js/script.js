cc0f917e28497eec33f52fc481fd3a188c37b4c9
Rogerio Bispo <silva_rogeriobispo@outlook.com>
2023-11-16 20:36:08 -0300

    js atz2

--- js/script.js
+++ js/script.js
@@ -38,3 +38,21 @@ function eventoPerguntas(pergunta) {
   pergunta.addEventListener("click", ativarPergunta);
 }
 perguntas.forEach(eventoPerguntas);
+
+// ativar galeria
+
+const galeria = document.querySelectorAll(".galeria img");
+const galeriaContainer = document.querySelector(".galeria");
+
+function ativarGaleria(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+function eventoGaleria(img) {
+  img.addEventListener("click", ativarGaleria);
+}
+
+galeria.forEach(eventoGaleria);
